3d2e6c8f75c991b43de8909b3b661e815b426570
Peter <peter@chromebook>
2024-12-23 19:26:12 +0100

    IT-83 coverage 31%

--- build.sh
+++ build.sh
@@ -4,5 +4,6 @@ export PYTHONPATH=.
 pip install -r requirements.txt
 pip list --outdated
 pylint *.py
-pytest tests
+coverage run -m pytest tests
+coverage report -m
 docker build --tag verjaardagskalender .
